fe21b6a7b955fb942bbd85a6f053fac1a30dfdc3
Oscar Riva <sezione9@gmail.com>
2015-12-10 11:18:55 +0100

    Update create.blade.php

--- resources/views/books/create.blade.php
+++ resources/views/books/create.blade.php
@@ -30,6 +30,42 @@
                     </div>
                     {!! Form::close() !!}
                 </div>
+                
+                <div class="panel-body">
+                    <!-- Display Validation Errors -->
+                    @include('common.errors')
+
+                    {!! Form::open(array('action' => 'AuthorController@store', 'class' => 'form-horizontal')) !!}
+
+                    <div class="form-group">
+                        {!! Form::label('author-first_name', 'Nome', array('class' => 'col-sm-3 control-label')) !!}
+                        <div class="col-sm-6">
+                            {!! Form::text('first_name', Input::old('first_name'), array('class' => 'form-control')) !!}
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        {!! Form::label('author-last_name', 'Cognome', array('class' => 'col-sm-3 control-label')) !!}
+                        <div class="col-sm-6">
+                            {!! Form::text('last_name', Input::old('last_name'), array('class' => 'form-control')) !!}
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        {!! Form::label('author-bio', 'Bio', array('class' => 'col-sm-3 control-label')) !!}
+                        <div class="col-sm-6">
+                            {!! Form::textarea('bio', Input::old('bio'), array('class' => 'form-control', 'rows' => 4, 'cols' => 50)) !!}
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <div class="col-sm-offset-3 col-sm-6">
+                        {!! Form::submit('Salva', array('class' => 'btn btn-default')) !!}
+                        </div>
+                    </div>
+                    {!! Form::close() !!}
+                </div>
+                
             </div>
         </div>
     </div>
