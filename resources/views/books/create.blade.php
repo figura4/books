b786fbb6b9bbfc3278c4b664cea86537d4014da5
Oscar Riva <sezione9@gmail.com>
2015-12-10 11:55:11 +0100

    Update create.blade.php

--- resources/views/books/create.blade.php
+++ resources/views/books/create.blade.php
@@ -20,42 +20,11 @@
                             {!! Form::text('italian_title', Input::old('italian_title'), array('class' => 'form-control')) !!}
                         </div>
                     </div>
-
-                    {!! echo Form::select('author_id', $author_list, null, ['class' => 'form-control']); !!}
-
+                    
                     <div class="form-group">
-                        <div class="col-sm-offset-3 col-sm-6">
-                        {!! Form::submit('Salva', array('class' => 'btn btn-default')) !!}
-                        </div>
-                    </div>
-                    {!! Form::close() !!}
-                </div>
-                
-                <div class="panel-body">
-                    <!-- Display Validation Errors -->
-                    @include('common.errors')
-
-                    {!! Form::open(array('action' => 'AuthorController@store', 'class' => 'form-horizontal')) !!}
-
-                    <div class="form-group">
-                        {!! Form::label('author-first_name', 'Nome', array('class' => 'col-sm-3 control-label')) !!}
-                        <div class="col-sm-6">
-                            {!! Form::text('first_name', Input::old('first_name'), array('class' => 'form-control')) !!}
-                        </div>
-                    </div>
-
-                    <div class="form-group">
-                        {!! Form::label('author-last_name', 'Cognome', array('class' => 'col-sm-3 control-label')) !!}
-                        <div class="col-sm-6">
-                            {!! Form::text('last_name', Input::old('last_name'), array('class' => 'form-control')) !!}
-                        </div>
-                    </div>
-
-                    <div class="form-group">
-                        {!! Form::label('author-bio', 'Bio', array('class' => 'col-sm-3 control-label')) !!}
-                        <div class="col-sm-6">
-                            {!! Form::textarea('bio', Input::old('bio'), array('class' => 'form-control', 'rows' => 4, 'cols' => 50)) !!}
-                        </div>
+                        {!! Form::label('book-author', 'Autore', array('class' => 'col-sm-3 control-label')) !!}
+                        {!! echo Form::select('author_id', $author_list, null, ['class' => 'form-control']); !!}
+                        Se non trovi l'autore in elenco, {{ HTML::link('/author/create', 'creane uno nuovo')}}
                     </div>
 
                     <div class="form-group">
@@ -65,7 +34,6 @@
                     </div>
                     {!! Form::close() !!}
                 </div>
-                
             </div>
         </div>
     </div>
