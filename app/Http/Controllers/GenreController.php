925d57b095c5e4b2056f16e90ac1bac08d9f2511
Oscar Riva <sezione9@gmail.com>
2015-12-10 08:27:51 +0100

    Update GenreController.php

--- app/Http/Controllers/GenreController.php
+++ app/Http/Controllers/GenreController.php
@@ -5,6 +5,9 @@ use App\Genre;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use App\Repositories\GenreRepository;
+use Log;
+use Input;
+
 class GenreController extends Controller
 {
     /**
@@ -35,5 +38,71 @@ class GenreController extends Controller
         return view('genres.index', [
             'genres' => $this->genres->getAll(),
         ]);
+    }
+    /**
+     * Create new genre.
+     *
+     * @param  Request  $request
+     * @return Response
+     */
+    public function create(Request $request)
+    {
+        return view('genres.createOrUpdate');
+    }
+    /**
+     * Edit existing genre.
+     *
+     * @param  Request  $request
+     * @param  Genre   $genre 
+     * @return Response
+     */
+    public function edit(Request $request, Genre $genre)
+    {
+        return view('genres.createOrUpdate')->with('genre', $genre);
+    }
+    /**
+     * Create a new genre.
+     *
+     * @param  Request  $request
+     * @return Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'last_name' => 'required|max:100',
+        ]);
+        $genre = new Genre($request->all());
+        $genre->save();
+        return redirect('/genre');
+    }
+    
+    /**
+     * Update existing genre.
+     *
+     * @param  Request  $request
+     * @param  Genre   $genre 
+     * @return Response
+     */
+    public function update(Request $request, Genre $genre)
+    {
+        $this->validate($request, [
+            'last_name' => 'required|max:100',
+        ]);
+        $input = array_except(Input::all(), '_method');
+	    $genre->update($input);
+	    
+        return redirect('/genre');
+    }
+    /**
+     * Destroy the given genre.
+     *
+     * @param  Request  $request
+     * @param  Genre  $genre
+     * @return Response
+     */
+    public function destroy(Request $request, Genre $genre)
+    {
+        $author->delete();
+        return redirect('/genre');
     }
 }
