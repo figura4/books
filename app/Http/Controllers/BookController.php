c98fd88d6a75efaed8809fac551533b2fcbe94cc
Oscar Riva <sezione9@gmail.com>
2015-12-10 10:24:34 +0100

    Update BookController.php

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -5,6 +5,9 @@ use App\Book;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use App\Repositories\BookRepository;
+use Log;
+use Input;
+
 class BookController extends Controller
 {
     /**
@@ -35,5 +38,71 @@ class BookController extends Controller
         return view('books.index', [
             'books' => $this->books->getAll(),
         ]);
+    }
+    /**
+     * Create new book.
+     *
+     * @param  Request  $request
+     * @return Response
+     */
+    public function create(Request $request)
+    {
+        return view('book.createOrUpdate');
+    }
+    /**
+     * Edit existing book.
+     *
+     * @param  Request  $request
+     * @param  Book   $book 
+     * @return Response
+     */
+    public function edit(Request $request, Book $book)
+    {
+        return view('books.createOrUpdate')->with('book', $book);
+    }
+    /**
+     * Create a new book.
+     *
+     * @param  Request  $request
+     * @return Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'italian_title' => 'required',
+        ]);
+        $book = new Book($request->all());
+        $book->save();
+        return redirect('/book');
+    }
+    
+    /**
+     * Update existing book.
+     *
+     * @param  Request  $request
+     * @param  Book   $book 
+     * @return Response
+     */
+    public function update(Request $request, Book $book)
+    {
+        $this->validate($request, [
+            'italian_title' => 'required',
+        ]);
+        $input = array_except(Input::all(), '_method');
+	    $book->update($input);
+	    
+        return redirect('/book');
+    }
+    /**
+     * Destroy the given book.
+     *
+     * @param  Request  $request
+     * @param  Author  $author
+     * @return Response
+     */
+    public function destroy(Request $request, Book $book)
+    {
+        $book->delete();
+        return redirect('/book');
     }
 }
