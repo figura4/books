ab7f95b69134ce7e271a58d27a6ff13e04693b50
Oscar Riva <sezione9@gmail.com>
2015-12-16 13:44:03 +0100

    Update create.blade.php

--- resources/views/books/create.blade.php
+++ resources/views/books/create.blade.php
@@ -11,20 +11,72 @@
                 <div class="panel-body">
                     <!-- Display Validation Errors -->
                     @include('common.errors')
+                    
+                    @if(isset($book))
+                        {!! Form::model($book, array('route' => array('book.update', $book->id), 'method' => 'PUT', 'class' => 'form-horizontal')) !!}
 
-                    {!! Form::open(array('action' => 'BookController@store', 'class' => 'form-horizontal')) !!}
-
+                    @else
+                        {!! Form::open(array('action' => 'BookController@store', 'class' => 'form-horizontal')) !!}
+                    @endif
+                    
                     <div class="form-group">
-                        {!! Form::label('book-italian_title', 'Titolo', array('class' => 'col-sm-3 control-label')) !!}
+                        {!! Form::label('book-italian_title', 'Titolo (*)', array('class' => 'col-sm-3 control-label')) !!}
                         <div class="col-sm-6">
                             {!! Form::text('italian_title', Input::old('italian_title'), array('class' => 'form-control')) !!}
                         </div>
                     </div>
                     
                     <div class="form-group">
+                        {!! Form::label('book-original_title', 'Titolo originale', array('class' => 'col-sm-3 control-label')) !!}
+                        <div class="col-sm-6">
+                            {!! Form::text('original_title', Input::old('original_title'), array('class' => 'form-control')) !!}
+                        </div>
+                    </div>
+                    
+                    <div class="form-group">
+                        {!! Form::label('book-pages', 'Pagine', array('class' => 'col-sm-3 control-label')) !!}
+                        <div class="col-sm-6">
+                            {!! Form::text('pages', Input::old('pages'), array('class' => 'form-control')) !!}
+                        </div>
+                    </div>
+                    
+                    <div class="form-group">
+                        {!! Form::label('book-editor', 'Editore', array('class' => 'col-sm-3 control-label')) !!}
+                        <div class="col-sm-6">
+                            {!! Form::text('editor', Input::old('editor'), array('class' => 'form-control')) !!}
+                        </div>
+                    </div>
+                    
+                    <div class="form-group">
+                        {!! Form::label('book-pub_year', 'Anno di pubblicazione', array('class' => 'col-sm-3 control-label')) !!}
+                        <div class="col-sm-6">
+                            {!! Form::text('pub_year', Input::old('pub_year'), array('class' => 'form-control')) !!}
+                        </div>
+                    </div>
+                    
+                    <div class="form-group">
+                        {!! Form::label('book-vote', 'Voto (*)', array('class' => 'col-sm-3 control-label')) !!}
+                        {!! Form::select('vote', [1, 2, 3, 4, 5], null, ['class' => 'form-control']) !!}
+                    </div>
+                    
+                    <div class="form-group">
                         {!! Form::label('book-author', 'Autore', array('class' => 'col-sm-3 control-label')) !!}
                         {!! Form::select('author_id', $author_list, null, ['class' => 'form-control']) !!}
                         Se non trovi l'autore in elenco, {!! HTML::link('/author/create', 'creane uno nuovo') !!}
+                    </div>
+                    
+                    <div class="form-group">
+                        {!! Form::label('book-review', 'Recensione (*)', array('class' => 'col-sm-3 control-label')) !!}
+                        <div class="col-sm-6">
+                            {!! Form::textarea('review', Input::old('review'), array('class' => 'form-control', 'rows' => 4, 'cols' => 50)) !!}
+                        </div>
+                    </div>
+                    
+                    <div class="form-group">
+                        {!! Form::label('book-review_pub_date', 'Data pubblicazione recensione', array('class' => 'col-sm-3 control-label')) !!}
+                        <div class="col-sm-6">
+                            {!! Form::text('review_pub_date', Input::old('review_pub_date'), array('class' => 'form-control')) !!}
+                        </div>
                     </div>
 
                     <div class="form-group">
